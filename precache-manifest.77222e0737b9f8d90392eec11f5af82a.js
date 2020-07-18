b6c23759a393585234f46ea5081dbacf4a418e12
Mike Vedsted <mikevedsted@gmail.com>
2020-07-18 22:15:55 +0300

    Updates

---
+++ precache-manifest.77222e0737b9f8d90392eec11f5af82a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3e74c0373b77ab02c277584b19bdcc",
+    "url": "/MikeVedsted/FaceFinder/index.html"
+  },
+  {
+    "revision": "d730a2d90541efd0e470",
+    "url": "/MikeVedsted/FaceFinder/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "85b20d85d68ce526ef5c",
+    "url": "/MikeVedsted/FaceFinder/static/css/main.a15752d8.chunk.css"
+  },
+  {
+    "revision": "d730a2d90541efd0e470",
+    "url": "/MikeVedsted/FaceFinder/static/js/2.c679e250.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MikeVedsted/FaceFinder/static/js/2.c679e250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b20d85d68ce526ef5c",
+    "url": "/MikeVedsted/FaceFinder/static/js/main.bd7bf7f9.chunk.js"
+  },
+  {
+    "revision": "96570f1007209d11afc9",
+    "url": "/MikeVedsted/FaceFinder/static/js/runtime-main.06264c3f.js"
+  }
+]);
